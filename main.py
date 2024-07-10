10ebe1ee6c2250eb21aeda15feb0c6228438ea1f
vii33 <@vii33_official>
2024-07-10 22:57:40 +0200

    refactored into seperate functions

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
-# Load credentials from .env file
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
-TOKEN = os.getenv('TOKEN')  # comes as external var e.g. .env file
-
-# ---- Bot ----
 import requests
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+from dotenv import load_dotenv
 
-# Get all chats
-result = requests.get(url).json() 
-print( result ) 
 
-# Retrieve chat ID (needed to send messages)
-chat_id = result['result'][0]['message']['chat']['id']
-print( f"\n  CHAT ID: {chat_id}" )
+def get_updates(token):
+    """Retrieve updates from the Telegram bot."""
+    url = f"https://api.telegram.org/bot{token}/getUpdates"
+    response = requests.get(url).json()
+    return response
 
-# Send message
-message = "hello from your telegram bot 2"
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-print( f"Send result: {requests.get(url).json()} ") # this sends the message
+def send_message(token, chat_id, message):
+    """Send a message to a specific chat using the Telegram bot."""
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+    response = requests.get(url).json()
+    return response
+
+
+
+if __name__ == "__main__":
+    # Load the token
+    load_dotenv()
+    TOKEN = os.getenv('TOKEN')
+
+    # Retrieve updates
+    updates = get_updates(TOKEN)
+    print(updates)
+
+    # Retrieve chat ID
+    chat_id = updates['result'][0]['message']['chat']['id']
+    print(f"\n  CHAT ID: {chat_id}")
+
+    # Send a message
+    message = "hello from your telegram bot 2"
+    send_result = send_message(TOKEN, chat_id, message)
+    print(f"Send result: {send_result}")
