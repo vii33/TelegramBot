386e3a1c1c4a2a39a37a631aa05ee0817ae245ff
vii33 <@vii33_official>
2024-07-11 20:35:55 +0200

    renamed file and updated function descriptions

---
+++ telegram_bot.py
@@ -0,0 +1,49 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+def load_bot_token_from_os() -> str:
+    """Loads the Telegram API Token from local .env file."""
+    load_dotenv()
+    return os.getenv('TOKEN')
+
+def get_updates(token:str):
+    """
+    Retrieve messages from Telegram bot.
+    
+    PARAMETERS
+        token: Telegram Bot Token
+    """
+    url = f"https://api.telegram.org/bot{token}/getUpdates"
+    response = requests.get(url).json()
+    return response
+
+def send_message(token:str, chat_id:int, message:str):
+    """
+    Send a message to a specific chat using the Telegram bot.
+    
+    PARAMETERS
+        token: Telegram Bot Token
+        chat_id:  ID of the specific private chat or group chat. Get ID from results or xxxxxxxx
+        message: Message text. Supports Markdown.
+    """
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}&parse_mode=MarkdownV2"
+    response = requests.get(url).json()
+    return response
+
+if __name__ == "__main__":
+    # Load the token
+    TOKEN = load_bot_token_from_os()
+
+    # Retrieve updates
+    updates = get_updates(TOKEN)
+    print(updates)
+
+    # Retrieve chat ID
+    chat_id = updates['result'][0]['message']['chat']['id']
+    print(f"\n  CHAT ID: {chat_id}")
+
+    # Send a message
+    message = "hello from your telegram bot 3\\. Dots have to be escaped! *Bold text*  __underline__   _italic text_"
+    send_result = send_message(TOKEN, chat_id, message)
+    print(f"Send result: {send_result}")
